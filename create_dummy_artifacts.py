6349f53ad396faff0b8ffcc7e7a40d82ef6dc583
Gorle-Varshini <gorlevarshini05@gmail.com>
2026-07-15 18:06:53 +0530

    Add files via upload

--- create_dummy_artifacts.py
+++ create_dummy_artifacts.py
@@ -3,15 +3,22 @@ from sklearn.preprocessing import StandardScaler
 import joblib
 import numpy as np
 
-# Dummy scaler trained on minimal data
+# Train scaler with 7 features
+X = np.array([
+    [0.02, 0.05, 25000, 45000000, 0, 0, 22],
+    [0.10, 0.20, 50000, 10000000, 0, 0, 18]
+])
+
 scaler = StandardScaler()
-scaler.fit(np.array([[0.0],[1.0]]))
+scaler.fit(X)
 
-# Dummy classifier that always predicts 0
-model = DummyClassifier(strategy='constant', constant=0)
-model.fit([[0,0,0,0,0,0,0]], [0])
+# Dummy model
+y = np.array([0, 1])
 
-joblib.dump(model, 'asteroid_model.pkl')
-joblib.dump(scaler, 'scaler.pkl')
+model = DummyClassifier(strategy="most_frequent")
+model.fit(X, y)
 
-print('Created asteroid_model.pkl and scaler.pkl')
+joblib.dump(model, "asteroid_model.pkl")
+joblib.dump(scaler, "scaler.pkl")
+
+print("Artifacts created successfully!")
